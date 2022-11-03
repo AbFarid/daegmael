79b9307bdcdb8c9d36ac7147e70e06044d101889
AbFarid <farid.abbasbayli@gmail.com>
2022-11-03 22:24:45 +0100

    Update all dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 var manifestJSON = require('./public/manifest.json');
 
-pwaArgs = {
+var pwaArgs = {
   themeColor: manifestJSON.theme_color,
   name: manifestJSON.short_name,
   msTileColor: manifestJSON.background_color
@@ -8,14 +8,8 @@ pwaArgs = {
 
 module.exports = {
   lintOnSave: false,
-  baseUrl: process.env.NODE_ENV === 'production' ? '/daegmael/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/daegmael/' : '/',
   chainWebpack: config => {
     config.plugin('pwa').tap(args => [pwaArgs]);
-
-    config.module
-      .rule('images')
-      .use('url-loader')
-      .loader('url-loader')
-      .tap(options => ({ ...options, limit: 6114, }));
   },
 };
